239bd98fbb2b6fa5cfe144dc5c4cac1022fecb4c
MS-vizja <166212512+MS-vizja@users.noreply.github.com>
2024-04-07 11:16:49 +0200

    main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+public static void main(String[] args) {
+    Point point = new Point(3.0, 2.0);
+        
+    System.out.println("wspolrzedna x = " + point.getX());
+    System.out.println("wspolrzedna y = " + point.getY());
+    }
+}
